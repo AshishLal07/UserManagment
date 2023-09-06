6645cbd4024a0ee618955f83b1966f7054caaad1
AshishLal07 <74709197+AshishLal07@users.noreply.github.com>
2023-09-06 23:46:22 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<>
-		<BrowserRouter>
+		<BrowserRouter basename="/UserManagment/">
 			<UserContextProvider>
 				<App />
 			</UserContextProvider>
